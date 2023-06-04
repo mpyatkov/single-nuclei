0c9dabf1e30698a79b046f20aaf5c3eae93fe409
mpyatkov <test@test.com>
2023-06-04 16:13:25 -0400

    Added new markers for heatmaps and dotplot, refactored heatmaps

--- bin/UmapPlot.R
+++ bin/UmapPlot.R
@@ -1,3 +1,55 @@
+## "Single-nucleus RNA sequencing of pre-malignant liver reveals disease-associated hepatocyte state with HCC prognostic potential"
+## Carlessi et al., 2023, Cell Genomics 3
+
+CELL_PAPER_MARKERS <- list(
+  `Hep`= c("Egfr","Pck1","Slc7a2","Cps1","Cyp7b1"),
+  `Mesenchy/HSC` = c("Dcn","Lsamp","Ank3","Col14a1", "Bmp5"),
+  `Endo` = c("Stab2","F8","Fgd5","Ptprb","Bmp6"),
+  `BECs/Cholang` = c("Pkhd1","Ctnnd2","Erbb4","Hnf1b"),
+  `Myeloid` = c("Cd5l","Adgre1","Myo1f","Clec4f","Sirpa"),
+  `B cells` = c("Ebf1","Pax5","Aff3","Prkcb"),
+  `T/NK Cells` = c("Skap1","Bcl11b","Lef1","Cd247"),
+  `pDCs` = c("Siglech","Runx2","Mctp2","Card11"),
+  `Mesothelial` = c("Bnc2","Sulf1","Grip1","Mast4"),
+  `daHep` = c("Abcc4","Cachd1","Ehhadh","lnc12608","Nt5e","Gsta1","Samd4","Gclc","Plin2","Nrg1",
+              "Spag5", "Taco1", "Klf6", "Mrnip","Gm28153","Mapt","Glis3","Robo1",
+              "Peak1","Igsf11","Dst","Dhx40","Slc7a11","Krt8") # lnc12608 = Pvt1
+)
+
+DEFAULT_MARKER_LIST = list(
+  `Standard biomarkers` = c('Cyp2e1', 'Glul', 'Oat', 'Gulo', 'Ass1', 'Hamp', 'Gstp1', 'Ubb', 'Selenbp2', 'Sds', 'Cyp2f2', 'Pck1',
+                         'Hal', 'Il2rb', 'Cxcr6', 'Gzma', 'Csf3r', 'S100a6', 'Pkhd1', 'Sox9', 'Epcam', 'Krt7', 'Krt19', 'Irf8',
+                         'Itgax', 'Clec4f', 'Csf1r', 'Jchain', 'Cd79a', 'Cd79b', 'Top2a', 'Stab2', 'Kdr', 'Aqp1', 'Fcgr2b',
+                         'Gpr182', 'Ebf1', 'Skap1', 'Ptprc', 'Ank3', 'Dcn', 'Colec11', 'Ecm1', 'Alb', 'Ttr', 'Apoa1', 'Serpina1c',
+                         'mt-Atp6', 'mt-Atp8', 'mt-Co3', 'mt-Co1', 'mt-Co2', 'mt-Nd2', 'mt-Nd4', 'mt-Cytb', 'mt-Nd1', 'mt-Nd3',
+                         'mt-Nd4l', 'mt-Nd5', 'mt-Nd6'),
+  
+  `PC(extreme)` = c("Slc1a2", "Slc16a10", "Tox", "Lhpp", "Tenm3", "Rcan2", "Slc22a1", "lnc13654"),
+  `HSC` = c("Ntm", "Nrxn1", "Ccbe1", "Gpc6", "Egfem1", "Hgf", "Zfp804b", "Nkain2"),
+  `Endo` = c("Meis2", "Fbxl7", "Plekhg1"), 
+  `Cholang` = c("Glis3", "Pdgfd"), 
+  `Dividing` = c("Cenpp","Ect2", "Tpx2", "Hmmr","Anln", "Diaph3"),
+  `Immune` = c("Inpp4b", "Bank1", "Fyb", "Hdac9", "Slc8a1") 
+)
+
+DEFAULT_HEATMAP_BIOMARKERS <- list(
+  PC = c("Glul","Gulo","Oat","Cyp2e1"),
+  PP = c("Pck1","Cyp2f2","Hal"),
+  Endo=c("Stab2","Meis2","Fbxl7","Ptprb","F8","Plekhg1", "Fgd5", "Bmp6"),
+  HSC=c("Dcn","Lsamp","Ank3","Col14a1", "Bmp5", "Ntm", "Nrxn1", "Ccbe1", "Gpc6", "Egfem1", "Hgf", "Zfp804b", "Nkain2"),
+  Kupffer = c("Clec4f","Csf1r","Bank1","Fyb", "Hdac9", "Slc8a1"),
+  Immune = c("Ebf1","Skap1","Inpp4b"), ##was only Ptprc
+  Cholang=c("Pkhd1","Glis3","Pdgfd","Erbb4", "Ctnnd2", "Hnf1b"), ## added Pkhd1  
+  Div=c("Top2a","Cenpp","Ect2","Tpx2","Hmmr","Anln","Diaph3"),
+  `PC(Ext)`=c("Slc1a2","Slc16a10","Tox","Lhpp","Tenm3","Rcan2","Slc22a1","lnc13654"),
+  `B cells` = c("Ebf1","Pax5","Aff3","Prkcb"),
+  `T/NK Cells` = c("Skap1","Bcl11b","Lef1","Cd247"),
+  `pDCs` = c("Siglech","Runx2","Mctp2","Card11"),
+  `Mesothelial` = c("Bnc2","Sulf1","Grip1","Mast4")
+)
+
+
+
 ### creates combined plot with layout (umap/table/dotplot)
 combinedUmapTableDotplot <- function(seurat_obj, TITLE, labels_for_umap = NULL, genes_list, cluster_stats, sample_id = "all") {
   
@@ -43,6 +95,47 @@ combinedUmapTableDotplot <- function(seurat_obj, TITLE, labels_for_umap = NULL, 
   combined_all_clusters
 }
 
+combinedUmapTable_v2 <- function(seurat_obj, TITLE, labels_for_umap = NULL, cluster_stats, sample_id = "all") {
+  
+  #### table for plots
+  table_for_plots <- cluster_stats %>% 
+    select(seurat_clusters, sample_id, ncells)
+  
+  ## dotplot for all samples together
+  DefaultAssay(seurat_obj) <- "RNA"
+  gc()
+  
+  
+  if (!is.null(labels_for_umap)) {
+    ## rename clusters (set new_labels)
+    seurat_obj <- RenameIdents(seurat_obj, labels_for_umap$new_labels)
+  }
+  
+  ## Plot all samples together on one UMAP without labels
+  aggr_umap_all_clusters <- mp_plot_umap(seurat_obj, title = TITLE, draw_labels = T)
+  
+  if (!is.null(labels_for_umap)) {
+    ## rename clusters back (set old_labels)
+    seurat_obj <- RenameIdents(seurat_obj, labels_for_umap$old_labels)
+  }
+  
+
+  clusters_stats_table_plot <- plot_table(get_table_by_id(sample_id, table_for_plots), title = "")
+  
+  ## set layout for 11 plots
+  layout <- "
+    AAAAB
+    AAAAB
+    AAAAB
+    "
+  combined_all_clusters <- cowplot::plot_grid((aggr_umap_all_clusters+theme(plot.margin=margin(0,1,0,0,"cm")))+
+                                                clusters_stats_table_plot+
+                                                plot_layout(design = layout)+plot_annotation(title = sample_id))
+  combined_all_clusters
+}
+  
+  
+  
 #' Supplementary function to create UMAP plots
 #'
 #' @param seurat_obj - Seurat object with predefined clusters
@@ -162,7 +255,12 @@ UmapDotplotTableHeatmapPlot <- function(seurat_obj,
   #### PREDICT CELL TYPES
   ### Assign predicted cell types labels
   ## get new_labels,old_labels,heatmap_table
-  celltypes_list <- FindCellTypesByMarkers(sobj = seurat_obj, heatmap_markers = NULL)
+  
+  if(is.null(heatmap_markers)){
+    heatmap_markers <-  DEFAULT_HEATMAP_BIOMARKERS
+  }
+  
+  celltypes_list <- FindCellTypesByMarkers(sobj = seurat_obj, biomarkers = heatmap_markers)
   celltypes_heatmap <- CreateCellTypesHeatmap(celltypes_list$heatmap_table)
   
   # ## dotplot for all samples together
@@ -215,8 +313,116 @@ AAAAA####
   
 }
 
+UmapDotplotTableHeatmapPlot_v2 <- function(seurat_obj, 
+                                 genes_list = NULL,
+                                 additional_genes_list = NULL,
+                                 sample_id = "all", 
+                                 TITLE = "",
+                                 heatmap_markers = NULL,
+                                 rename_umap_labels = T) {
+  
+  #### PREDICT CELL TYPES
+  ### Assign predicted cell types labels
+  ## get new_labels,old_labels,heatmap_table
+
+  if(is.null(heatmap_markers)){
+    heatmap_markers <-  DEFAULT_HEATMAP_BIOMARKERS
+  }
+  
+  if (is.null(genes_list)) {
+    genes_list <- DEFAULT_MARKER_LIST
+  }
+  
+  celltypes_list <- FindCellTypesByMarkers(sobj = seurat_obj, biomarkers = heatmap_markers)
+  celltypes_heatmap <- CreateCellTypesHeatmap(celltypes_list$heatmap_table, celltype_order = names(heatmap_markers), rotate_x = T)
+  
+  ## CREATE UMAP
+  DefaultAssay(seurat_obj) <- "RNA"
+
+  if (rename_umap_labels) {
+      #### ADD LABELS FOR UMAP IF EXISTS
+      ## rename clusters (set new_labels)
+      seurat_obj <- RenameIdents(seurat_obj, celltypes_list$new_labels)
+
+      ## Plot all samples together on one UMAP without labels
+      aggr_umap_all_clusters <- mp_plot_umap(seurat_obj, title = TITLE, draw_labels = T)
+      
+      ## rename clusters back (set old_labels)
+      seurat_obj <- RenameIdents(seurat_obj, celltypes_list$old_labels)
+  } else {
+      ## just plot UMAP
+      aggr_umap_all_clusters <- mp_plot_umap(seurat_obj, title = TITLE, draw_labels = T)
+  }
+    
+  #### CREATE DOTPLOT
+  ## TODO: at the moment all genes will be represented on the dotplots
+  default_dotplot <- mp_dotplot(seurat_obj, genes_list)
+  
+  ##
+  additional_dotplot <- NULL
+  ## Additional dotplot
+  if (!is.null(additional_genes_list)){
+    additional_dotplot <- mp_dotplot(seurat_obj, additional_genes_list)
+  }
+  
+  #### CREATE STATS TABLE WITH CLUSTERS
+  clusters_stats_table_plot <- mp_plot_table(list_sample_cluster_stats(seurat_obj)[[sample_id]], title = "")
+  
+  ## LAYOUTS
+  umap_stats_layout <- "
+    AAAA#B
+    AAAA#B
+    AAAA#B
+    "
+  
+  ## UMAP-STATS
+  umap_stats_plot <- cowplot::plot_grid((aggr_umap_all_clusters+theme(plot.margin=margin(0,1,0,0,"cm")))+
+                                                clusters_stats_table_plot+
+                                                plot_layout(design = umap_stats_layout)+plot_annotation(title = sample_id))
+  
+  #### ATTACH HEATMAP TO UmapStatsDotplot plot
+  umap_stats_heatmap_layout1 <- "
+AAAAA#####
+AAAAA#BBBB
+AAAAA#BBBB
+AAAAA#####
+CCCCCCCCCC
+"
+
+  ### for additional dotplot
+  umap_stats_heatmap_layout2 <- "
+AAAAA#####
+AAAAA#BBBB
+AAAAA#BBBB
+AAAAA#BBBB
+AAAAA#####
+CCCCCCCCCC
+DDDDDDDDDD
+"
+
+  umap_stats_heatmap_dotplot_plot <- if(is.null(additional_genes_list)){
+    umap_stats_plot+
+      celltypes_heatmap+
+      default_dotplot+
+      plot_layout(design = umap_stats_heatmap_layout1)
+
+  } else {
+    umap_stats_plot+
+      celltypes_heatmap+
+      default_dotplot+
+      additional_dotplot+
+      plot_layout(design = umap_stats_heatmap_layout2)
+  }
+
+  umap_stats_heatmap_dotplot_plot
+}
+
 ## Predict celltypes by set of markers
-FindCellTypesByMarkers <- function(sobj, heatmap_markers = NULL) {
+FindCellTypesByMarkers <- function(sobj, biomarkers = NULL) {
+  
+  if(is.null(biomarkers)) {
+    stop("parameter biomarkers should be specied (now NULL)")
+  }
   
   ## If function can not find any significant genes for several cluster
   ## then heatmap should show all zeros for such clusters for each celltype
@@ -225,21 +431,23 @@ FindCellTypesByMarkers <- function(sobj, heatmap_markers = NULL) {
   ## !! If it was detected only one celltype and even if the expression is negative for this celltye
   ## the procedure anyway assign this celltype for cluster because other is zero
   
-  biomarkers <- heatmap_markers
-  
-  if (is.null(heatmap_markers)) {
-    biomarkers <- list(
-      PC = c("Glul","Gulo","Oat","Cyp2e1"),
-      PP = c("Pck1","Cyp2f2","Hal"),
-      Kupffer = c("Clec4f","Csf1r"),
-      Immune = c("Ptprc"),
-      HSC=c("Colec11","Dcn","Ecm1"),
-      Endo=c("Stab2","Gpr182","Kdr","Fcgr2b","Aqp1"),
-      Div=c("Top2a"),
-      Cholang=c("Epcam","Krt19","Krt7","Sox9")
-    )
-  }
-  
+  # biomarkers <- heatmap_markers
+  # 
+  # if (is.null(heatmap_markers)) {
+  #   biomarkers <- list(
+  #     PC = c("Glul","Gulo","Oat","Cyp2e1"),
+  #     PP = c("Pck1","Cyp2f2","Hal"),
+  #     Kupffer = c("Clec4f","Csf1r","Bank1","Fyb", "Hdac9", "Slc8a1"),
+  #     Immune = c("Ebf1","Skap1","Inpp4b"), ##was only Ptprc
+  #     HSC=c("Colec11","Dcn","Ecm1","Ntm","Nrxn1","Ccbe1","Zfpm2","Gpc6","Egfem1","Lsamp","Hgf","Zfp804b","Bmp5","Nkain2"), 
+  #     Endo=c("Stab2","Gpr182","Kdr","Fcgr2b","Aqp1","Meis2","Fbxl7","Ptprb","F8","Plekhg1"),
+  #     Div=c("Top2a","Cenpp","Ect2","Tpx2","Hmmr","Anln","Diaph3"),
+  #     Cholang=c("Epcam","Krt19","Krt7","Sox9","Pkhd1","Bicc1","Glis3","Pdgfd","Erbb4","Thsd4"), ## added Pkhd1
+  #     Ext_PC=c("Slc1a2","Slc16a10","Tox","Lhpp","Tenm3","Rcan2","Slc22a1","lnc13654")
+  #   )
+  # }
+  # 
+
   DefaultAssay(sobj) <- "RNA"
   
   tmp <- map_dfr(names(biomarkers), function(name) {
@@ -265,7 +473,7 @@ FindCellTypesByMarkers <- function(sobj, heatmap_markers = NULL) {
   ## FindAllMarkers skips low expressed genes and produces as output empty rows for some celltypes
   ## To make full table without empty rows we have to append celltypes with zero scores for such of celltypes
   
-  tmp <- left_join(tmp %>% expand(cluster,celltype), tmp) %>% 
+  tmp <- left_join(tmp %>% tidyr::expand(cluster,celltype), tmp) %>% 
     replace_na(list(score = 0))
   
   labels <- tmp %>% 
@@ -342,14 +550,19 @@ list_sample_cluster_stats <- function(seurat_obj){
 }
 
 ## create heatmap by table (cluster, celltype, score)
-CreateCellTypesHeatmap <- function(df, labels_text_size = 6, xaxis_text_size = 15, yaxis_text_size = 15, rotate_x = FALSE){
+CreateCellTypesHeatmap <- function(df, labels_text_size = 6, xaxis_text_size = 15, yaxis_text_size = 15, rotate_x = FALSE, celltype_order = NULL){
+  
+  if(!is.null(celltype_order)){
+    df$celltype = factor(df$celltype, levels = celltype_order, ordered = TRUE)
+  }
+  
   tmp <- ggplot(df, aes(x = celltype, y = as.factor(cluster))) +
     geom_tile(aes(fill = score),color= "gray50",size = 0.1)+
     scale_fill_gradient2(low = "blue", mid="white", high = "tomato")+
     geom_text(aes(label=round(score,2)), size = labels_text_size)+
     scale_x_discrete(position = "top") +
-    xlab("")+
-    ylab("")+
+    xlab("Markers")+
+    ylab("Clusters")+
     theme_minimal() +
     theme(legend.title = element_blank(),
           legend.position = "bottom",
@@ -357,7 +570,9 @@ CreateCellTypesHeatmap <- function(df, labels_text_size = 6, xaxis_text_size = 1
           axis.text.y = element_text(color = "black", size = yaxis_text_size))
   
   if (rotate_x){
-    tmp <- tmp+theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1,color = "black", size = xaxis_text_size))
+    # tmp <- tmp+theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1,color = "black", size = xaxis_text_size))
+    tmp <- tmp+theme(axis.text.x = element_text(angle = 90, hjust=0, color = "black", size = xaxis_text_size),
+                     axis.text.x.top = element_text(vjust = 0.5))
   }
   tmp
 }
